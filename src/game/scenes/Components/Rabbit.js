06f1d59dec7792889c8813e14bd2de98f11c8d0a
Carlos <c.pena17@ufromail.cl>
2024-04-20 22:16:38 -0400

    added diagrams and remove component

--- src/game/scenes/Components/Rabbit.js
+++ src/game/scenes/Components/Rabbit.js
@@ -4,27 +4,23 @@ export default class Conejo extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, texture) {
     super(scene, x, y, texture);
 
-    // Añadir a la escena
     scene.add.existing(this);
     scene.physics.add.existing(this);
 
-    // Configuraciones físicas básicas
     this.setCollideWorldBounds(true);
     this.setBounce(0.8);
     this.setInteractive();
 
-    // Configuración de la interactividad
     this.on('pointerdown', () => {
       this.clickHandler();
+      
     });
 
-    // Movimiento inicial o cualquier otro setup necesario
     this.initMovement();
   }
 
   initMovement() {
-    // Aquí puedes definir cómo se mueve inicialmente el conejo,
-    // por ejemplo, un impulso inicial o una dirección aleatoria.
+
     const jumpPower = 700;
     this.setVelocityY(-jumpPower);
 
@@ -34,7 +30,7 @@ export default class Conejo extends Phaser.Physics.Arcade.Sprite {
   }
 
   clickHandler() {
-    // Define lo que sucede cuando el conejo es clickeado
+
     this.destroy();
   }
 }
--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -8,18 +8,7 @@ export class Game extends Scene {
   }
   preload() {
 
-    const gameWidth = this.sys.game.config.width;
-    const gameHeight = this.sys.game.config.height+1;
-
-    const image = this.add.image(gameWidth / 2, gameHeight / 2, 'grass');
-
-    const imageWidth = image.width;
-    const imageHeight = image.height;
-
-    const scaleX = gameWidth / imageWidth;
-    const scaleY = gameHeight / imageHeight;
-
-    image.setScale(scaleX, scaleY);
+   this.scaleBackground();
 
   }
   create() {
@@ -38,11 +27,25 @@ export class Game extends Scene {
 
     this.physics.world.setBounds(0, 0, this.sys.game.config.width, this.sys.game.config.height)
 
-   const rabbit = new Rabbit(this, startX, startY, 'star'); // Asegúrate de que 'star' sea cambiado por el asset correcto para el conejo
+   const rabbit = new Rabbit(this, startX, startY, 'star'); 
   }
 
   changeScene() {
     this.scene.start('GameOver')
+  }
+  scaleBackground() {
+    const gameWidth = this.sys.game.config.width;
+    const gameHeight = this.sys.game.config.height+1;
+
+    const image = this.add.image(gameWidth / 2, gameHeight / 2, 'grass');
+
+    const imageWidth = image.width;
+    const imageHeight = image.height;
+
+    const scaleX = gameWidth / imageWidth;
+    const scaleY = gameHeight / imageHeight;
+
+    image.setScale(scaleX, scaleY);
   }
 }
 
